d09e637f50bc3553751effe009ec0fcdd2639c81
tlujan94 <tlujan94@rams.colostate.edu>
2022-09-16 15:24:11 -0600

    implement error handling

--- boyos.cpp
+++ boyos.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 #include "src/engine.h"
+#include "src/errors/divide_by_zero.h"
+#include "src/errors/invalid_operand.h"
+#include "src/errors/invalid_operator.h"
+#include "src/errors/missing_operand.h"
+#include "src/errors/missing_operator.h"
 
 using namespace std;
 
@@ -9,7 +14,18 @@ int main(int argc, const char** argv) {
         engine engine;
         int result = engine.execute(argc, argv);
         cout << result << endl;
+    } catch (divide_by_zero& ex) {
+        cout << "error: " << ex.what() << endl;
+    } catch (invalid_operand& ex) {
+        cout << "error: " << ex.what() << endl;
+    } catch (invalid_operator& ex) {
+        cout << "error: " << ex.what() << endl;
+    } catch (missing_operand& ex) {
+        cout << "error: " << ex.what() << endl;
+    } catch (missing_operator& ex) {
+        cout << "error: " << ex.what() << endl;
     } catch (...) {
-        cout << "error" << endl;
+        cout << "fatal error" << endl;
+        exit(1);
     }
 }
