28bc8d06306b3cc3bb10b1b349b9176108e42ade
tlujan94 <tlujan94@rams.colostate.edu>
2022-09-16 15:01:01 -0600

    integrate engine in boyos main

--- boyos.cpp
+++ boyos.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include "src/engine.h"
+
 using namespace std;
 
-int main() {
-    cout << "Hello, world!" << endl;
+int main(int argc, const char** argv) {
+    try {
+        engine engine;
+        int result = engine.execute(argc, argv);
+        cout << result << endl;
+    } catch (...) {
+        cout << "error" << endl;
+    }
 }
